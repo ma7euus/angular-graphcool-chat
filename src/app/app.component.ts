2373aa1e1f45de2f3291e1d98a52a96be918b3a7
Mateus <mateuspastori@outlook.com>
2019-05-21 23:48:02 -0300

    apollo

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,9 +8,13 @@ import {HttpClient} from '@angular/common/http';
 })
 export class AppComponent {
 
+    private apiURL = 'https://api.graph.cool/simple/v1/cjvrdvhp52xr80139ur1w05ya';
+
     constructor(
         private http: HttpClient
     ) {
+      this.createUser();
+      this.allUsers();
     }
 
     allUsers(): void {
@@ -23,8 +27,26 @@ export class AppComponent {
                 }
             }`
         };
-      //  this.http.post();
+        this.http.post(this.apiURL, body)
+        .subscribe(res => console.log('Query:', res));
     }
 
-
+    createUser(): void {
+      const body = {
+        query: `mutation CreateNewUser($name: String!, $email: String!, $password: String!) {
+          createUser(name: $name, email: $email, password: $password) {
+            name
+            email
+            password
+          }
+        }`,
+        variables: {
+          name: 'Alana Angst',
+          email: 'alaninha@hotmail.com',
+          password: '123123123'
+        }
+      };
+      this.http.post(this.apiURL, body)
+      .subscribe(res => console.log('Mutation:', res));
+    }
 }
