8f8365b1a8220d433f887b629d0791cbea100598
Mateus <mateuspastori@outlook.com>
2019-05-23 13:33:33 -0300

    apollo

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,7 +3,7 @@ import {MatToolbarModule, MatListModule} from '@angular/material';
 
 import {AppComponent} from './app.component';
 import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
-import {HttpClientModule} from '@angular/common/http';
+import {ApolloConfigModule} from './apollo.config.module';
 
 @NgModule({
     declarations: [
@@ -11,7 +11,7 @@ import {HttpClientModule} from '@angular/common/http';
     ],
     imports: [
         BrowserAnimationsModule,
-        HttpClientModule,
+        ApolloConfigModule,
         MatListModule,
         MatToolbarModule
     ],
